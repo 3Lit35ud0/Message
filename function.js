246e463a3ed79c3c3d75ef70ca97049c59f4a9a6
3Lit35ud0 <manoinsrm@gmail.com>
2021-07-05 01:14:35 -0700

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 alphabet = new Array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
 letter_count = 0;
-el = $("#Coming Soon!!");
+el = $("#ComingSoon!!");
 word = el.html().trim();
 finished = false;
 
